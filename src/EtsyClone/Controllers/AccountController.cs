e7bd0b727f864022687cc7952b36dc64779f6780
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-16 11:10:30 -0700

    Create Account Controller, Create Login, LoginViewmOdel, IdentityExtensions

---
+++ src/EtsyClone/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EtsyClone.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EtsyClone.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ src/EtsyClone/Models/IdentityExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtsyClone.Models
+{
+    public static class IdentityExtensions
+    {
+        public static async Task<ApplicationUser> FindByNameOrEmailAsync
+            (this UserManager<ApplicationUser> userManager, string usernameOrEmail, string password)
+        {
+            var username = usernameOrEmail;
+            if (usernameOrEmail.Contains("@"))
+            {
+                var userForEmail = await userManager.FindByEmailAsync(usernameOrEmail);
+                if (userForEmail != null)
+                {
+                    username = userForEmail.UserName;
+                }
+            }
+            return await userManager.FindAsync(username, password);
+        }
+    }
+}
