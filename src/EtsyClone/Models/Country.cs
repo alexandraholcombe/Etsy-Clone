3ca424119bf971414045626fe8dfc3cfb6406d8f
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-17 14:51:22 -0700

    Add restsharp and newtonsoft to dependencies

---
+++ src/EtsyClone/Models/Country.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EtsyClone.Models
+{
+    public class Country
+    {
+        public int country_id { get; set; }
+        public string name { get; set; }
+
+        public static List<Country> GetCountries()
+        {
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("users/alexandraholcombe/starred?sort=created&direction=asc&per_page=3", Method.GET);
+            request.AddHeader("User-Agent", "alexandraholcombe");
+            request.AddHeader("Accept", "application/json");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            var projectList = JsonConvert.DeserializeObject<List<Project>>(jsonResponse.ToString());
+            return projectList;
+
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+
+    }
+}
